3bbe18847f660905e9ff584c561e424ef7eba022
이수민(Soomin Lee) <71430607+SL-ee@users.noreply.github.com>
2021-08-31 14:58:01 +0900

    Create network_node.py

---
+++ making_nodes_edges/network_node.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+network = pd.read_excel("D:/listToNetwork.xlsx")
+
+# 선언
+num = list(range(1, len(network.index))
+label = []
+weight = []
+
+# 열 개수 df1.shape[1] - 1 열은 1부터
+# print(network.shape[1] - 1)
+
+# 297개
+for i in range(0, network.shape[0]):
+    label.append(network.iloc[i][0])
+
+# 297개
+# for i in range(1, network.shape[1]):
+#     print(network.iloc[0][i])
+
+for i in range(0, network.shape[0]):
+    weight.append(network.iloc[i][i+1])
+
+excel_export = pd.DataFrame(
+    {
+        'id': num,
+        'label': label,
+        'index': num,
+        'weight': weight
+    })
+
+excel_export.to_excel("D:/node.xlsx", header=True, index=False)
